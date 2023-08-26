2f9c0b1ca49072a147ea47de1601a68faf94eddb
AmandaChristina <amanda.christina.n@gmail.com>
2023-08-26 16:09:24 -0300

    [Update]Movendo via distancia
    Alterei o funcionamento do move para a distancia percorrida.

--- Assets/Scripts/MovementBehavior.cs
+++ Assets/Scripts/MovementBehavior.cs
@@ -4,44 +4,49 @@ using UnityEngine;
 
 public class MovementBehavior : MonoBehaviour
 {
-    public Transform startMarker;
-    public Transform endMarker;
+    //public Transform startMarker;
+    //public Transform endMarker;
+    Vector3 startMarker;
+    [SerializeField] Vector3 endMarker;
     public float speed = 1.0F;
     private float startTime;
     private float journeyLength;
-    // Start is called before the first frame update
+
+
     void Start()
     {
         startTime = Time.time;
+        startMarker = transform.position;
+        endMarker = new Vector3(startMarker.x + endMarker.x, startMarker.y + endMarker.y, startMarker.z + endMarker.z);
+        journeyLength = Vector3.Distance(startMarker, endMarker);
+
+    }
         
-        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
 
-    }
-        public void MovementLoop(){
-     
-        print("Time: " + startTime);
-    
-
-        float distCovered = (Time.time - startTime) * speed;
-
-        float fractionOfJourney = distCovered / journeyLength;
-
-        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
-        print("Dist: " + fractionOfJourney);
-
-        if (fractionOfJourney >= 1){   
-            startTime = Time.time;
-            Transform holder = startMarker;
-            startMarker = endMarker;
-            endMarker = holder;
-        }
-    }
-
-    // Update is called once per frame
     void Update()
     {
         MovementLoop();
     }
 
+    public void MovementLoop()
+    {
+
+        print("Time: " + startTime);
+
+
+        float distCovered = (Time.time - startTime) * speed; //velocidade média
+        float fractionOfJourney = distCovered / journeyLength;
+
+        transform.position = Vector3.Lerp(startMarker, endMarker, fractionOfJourney);
+        print("Dist: " + fractionOfJourney);
+
+        if (fractionOfJourney >= 1)
+        {
+            startTime = Time.time;
+            Vector3 holder = startMarker;
+            startMarker = endMarker;
+            endMarker = holder;
+        }
+    }
 
 }
